47f495f323823b331ed8ddfe04cb428cece58166
Mina Shenouda <mina.hanna.morris@gmail.com>
2023-12-21 23:18:18 -0800

    install express-validation

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -9,7 +9,6 @@ export const Register = (req: Request, res: Response) => {
   if (error) {
     return res.status(400).send(error.details);
   }
-
   if (body.password !== body.passwordConfirm) {
     return res.status(400).send({
       message: 'ERROR :: Passwords do not match!'
