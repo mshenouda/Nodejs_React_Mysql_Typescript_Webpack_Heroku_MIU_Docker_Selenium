d2a206ff751f599c4fe0ba3b75b522f73943128c
Mina Shenouda <mina.hanna.morris@gmail.com>
2024-01-22 17:23:50 -0800

    Revert "False commit"
    This reverts commit cba7a708e855ac2fbd6a0d1d3845263c2474ea55.

--- backend/src/config/db.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "Crestlove_13",
-    DB: "reactapp"
-};
