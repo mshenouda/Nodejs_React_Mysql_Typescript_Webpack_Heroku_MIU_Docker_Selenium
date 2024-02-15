e13967abbed00867576332c87f0a904a5a93a818
Mina Shenouda <mina.hanna.morris@gmail.com>
2024-02-15 12:53:56 -0800

    heroku

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,15 +25,15 @@ export default class Server {
     //var publicPath = path.join(__dirname, '../../../frontend/build');
     if (process.env.NODE_ENV === "production") {
       app.use(express.static(path.join(__dirname, './build'), { maxAge: 30 * 60 * 60 * 24 * 1000 }));
-      app.get("/*", (req, res) => {
+      app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname,  "../../../frontend/build", "index.html"));
       });
     }
 
-    app.use(cors(corsOptions));
+    // app.use(cors(corsOptions));
     app.use(bodyParser.json({ type: 'application/*+json' }))
-    app.use(bodyParser.text({ type: 'text/html' }))
-    app.use(express.json());
+    // app.use(bodyParser.text({ type: 'text/html' }))
+    // app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
   }
 }
