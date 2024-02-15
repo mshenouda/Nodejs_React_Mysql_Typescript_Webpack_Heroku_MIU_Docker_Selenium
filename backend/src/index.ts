ce93123daf20fd57dd4c55b55ed5e6efc4ff3338
Mina Shenouda <mina.hanna.morris@gmail.com>
2024-02-15 02:46:04 -0800

    heroku

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,7 +24,7 @@ export default class Server {
 
     //var publicPath = path.join(__dirname, '../../../frontend/build');
     if (process.env.NODE_ENV === "production") {
-      app.use(express.static("build"));
+      app.use(express.static(path.join(__dirname, './build'), { maxAge: 30 * 60 * 60 * 24 * 1000 }));
       app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname,  "../../../frontend/build", "index.html"));
       });
