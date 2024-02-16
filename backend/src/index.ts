8d3dbf0b337079199e5ed0db54619932cb892477
Mina Shenouda <mina.hanna.morris@gmail.com>
2024-02-16 11:56:26 -0800

    heroku

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,11 +22,11 @@ export default class Server {
     const staticPath = path.join(__dirname, "../../../frontend/build");
     const publicPath = path.resolve(__dirname,  staticPath, "index.html");
     let corsOptions = {
-      origin: `http://${process.env.HOST}:${process.env.REACT_PORT}`
+      origin: `http://${process.env.REACT_APP_HOST}:${process.env.REACT_PORT}`
     };
     if (process.env.NODE_ENV === "production") {
       corsOptions = {
-        origin: `${process.env.HOST}`
+        origin: `${process.env.REACT_APP_HOST}`
       };
       app.use(express.static(staticPath, { maxAge: 30 * 60 * 60 * 24 * 1000 }));
       app.get("/", (req, res) => {
