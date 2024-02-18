2f0a56a4f3271455143ac6d9e99191c52f23da39
Mina Shenouda <mina.hanna.morris@gmail.com>
2024-02-18 07:58:08 -0800

    heroku

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -7,8 +7,8 @@ import userRoutes from "./user.routes";
 
 export default class Routes {
   constructor(app: Application) {
-    app.use("api", homeRoutes);
-    app.use("api/tutorials", tutorialRoutes);
+    app.use("not_api", homeRoutes);
+    app.use("notapi/tutorials", tutorialRoutes);
     app.use("api/loggers", loggerRoutes);
     app.use("api/users", userRoutes);
   }
