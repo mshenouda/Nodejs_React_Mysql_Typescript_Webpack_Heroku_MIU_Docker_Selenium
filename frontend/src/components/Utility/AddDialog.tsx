67d0b01b14596736a90700671e1396780faf82f6
Mina Shenouda <mina.hanna.morris@gmail.com>
2024-02-18 12:06:45 -0800

    heroku

--- frontend/src/components/Utility/AddDialog.tsx
+++ frontend/src/components/Utility/AddDialog.tsx
@@ -33,6 +33,7 @@ function SimpleDialog(props: SimpleDialogProps) {
     const handleDescription = (e: ChangeEvent<HTMLInputElement>) => setDescription(e.target.value);
     const handlePublished = (e: ChangeEvent<HTMLInputElement>) => setPublished(e.target.checked);
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
         const requestOptions = {
             method: 'POST',
             headers: {
